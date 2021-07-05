985761d0455a04a8f8aa37bfd2e36eb81c6d51aa
ronkXL <86980506+ronkXL@users.noreply.github.com>
2021-07-05 21:40:25 +0300

    LATENCY COMMAND

--- openbot.py
+++ openbot.py
@@ -7,4 +7,16 @@ client = commands.Bot(command_prefix = '--')
 async def on_ready():
     print('OpenBot#6578 is ready to use')
 
-client.run('your token here:D')
+@client.event
+async def on_member_join(member):
+    print (f'{member} has joined the squad')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the squad')
+
+@client.command()
+async def latency(ctx):
+    await ctx.send(f'Latency is {round(client.latency * 1000)}')
+
+client.run('')
